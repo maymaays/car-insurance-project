c97406cb2c63c6a4ada0dee9840b9e7210ddd53e
maymaays <methawee.a@ku.th>
2017-06-15 11:18:24 +0700

    mody get-res.php

--- js/function.js
+++ js/function.js
@@ -1,5 +1,20 @@
-function get_make() {
-  var id = event.currentTarget.attributes.id.value;
-  Cookies.set('make_id', id);
-  console.log(Cookies.get('make_id'));
+$(document).ready(function () {
+    $('#wait_for_model').hide();
+    $('#select_make').change(function () {
+        $('#wait_for_model').show();
+        $('#result_model').hide();
+        $.get("get_res.php", {
+            func: "make_option()",
+            drop_var: $('#select_make').val()
+        }, function (response) {
+            $('#result_model').fadeOut();
+            setTimeout("finish('result_model', '" + encodeURI(response) + "')", 400);
+        });
+        return false;
+    });
+});
+function finish(id, response) {
+    $('#wait_for_model').hide();
+    $('#' + id).html(decodeURI(response));
+    $('#' + id).fadeIn();
 }

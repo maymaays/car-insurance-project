26867fa838ebeca29b029097c47c7da45bce91e0
maymaays <methawee.a@ku.th>
2017-06-21 17:46:26 +0700

    add ajax

--- js/function.js
+++ js/function.js
@@ -1,20 +1,121 @@
-$(document).ready(function () {
-    $('#wait_for_model').hide();
-    $('#select_make').change(function () {
-        $('#wait_for_model').show();
-        $('#result_model').hide();
-        $.get("get_res.php", {
-            func: "make_option()",
-            drop_var: $('#select_make').val()
-        }, function (response) {
-            $('#result_model').fadeOut();
-            setTimeout("finish('result_model', '" + encodeURI(response) + "')", 400);
-        });
-        return false;
+window.onload = make();
+var store = this.value;
+
+function make() {
+    console.log('test make ♡');
+    $.ajax({
+        type: 'post',
+        url: 'php/get_res.php',
+        data: {
+            'key': 'select_make'
+        },
+        success: function (data) {
+            console.log(data);
+            var car_make = JSON.parse(data);
+            var $make = $('#select_make');
+            $make.empty();
+            $make.append('<option value="">select your make</option>');
+            for (var i = 0; i < car_make.length; i++) {
+                 $make.append('<option  value='+ car_make[i].carid +'>' + car_make[i].make + '</option>');
+                store = car_make.value;
+            }
+            $make.change();
+        }
     });
-});
-function finish(id, response) {
-    $('#wait_for_model').hide();
-    $('#' + id).html(decodeURI(response));
-    $('#' + id).fadeIn();
 }
+
+function models() {
+    console.log('test model ♡');
+    $.ajax({
+        type: 'post',
+        url: 'php/get_res.php',
+        data: {
+            'key': 'select_model',
+            'model': get_make(),
+        },
+        success: function (data) {
+            console.log(data);
+            var car_model = JSON.parse(data);
+            var $model = $('#select_model');
+            $model.empty();
+            $model.append('<option value="">select your model</option>');
+            for (var i = 0; i < car_model.length; i++) {
+                $model.append('<option  value='+ car_model[i].carid +'>' + car_model[i].model + '</option>');
+            }
+            $model.change();
+        }
+    });
+}
+
+function sub_model() {
+    console.log('test sub ♡');
+    $.ajax({
+        type: 'post',
+        url: 'php/get_res.php',
+        data: {
+            'key': 'select_sub',
+            'sub': get_model()
+        },
+        success: function (data) {
+            console.log(data);
+            var car_sub = JSON.parse(data);
+            var $sub = $('#select_sub_model');
+            $sub.empty();
+            $sub.append('<option value="">select your submodel</option>');
+            for (var i = 0; i < car_sub.length; i++) {
+                $sub.append('<option  value='+ car_sub[i].carid +'>' + car_sub[i].submodel + '</option>');
+            }
+            $sub.change();
+        }
+    });
+}
+
+function get_year() {
+    var selected_year = document.getElementById('select_year').options[document.getElementById('select_year').selectedIndex].text;
+    return 2017 - selected_year;
+}
+
+function get_make() {
+    var selected_make = document.getElementById('select_make').options[document.getElementById('select_make').selectedIndex].text;
+    return selected_make;
+}
+
+function get_model() {
+    var selected_model = document.getElementById('select_model').options[document.getElementById('select_model').selectedIndex].text;
+    return selected_model;
+}
+
+function get_sub() {
+    var selected_sub = document.getElementById('select_sub_model').options[document.getElementById('select_sub_model').selectedIndex].text;
+    return selected_sub;
+}
+
+function get_car() {
+    console.log('get car ♡');
+    $.ajax({
+        type: 'post',
+        url: 'php/get_res.php',
+        data: {
+            'key': 'output',
+            'make': get_make(),
+            'model': get_model(),
+            'sub': get_sub(),
+            'year': get_year()
+        },
+        success: function (data) {
+            console.log(data);
+            var car = JSON.stringify(data);
+            Cookies.set('car_data', car);
+            console.log('car from cookies: ' + Cookies.get('car_data'));
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
--- php/get_res.php
+++ php/get_res.php
@@ -2,32 +2,68 @@
 
 $db = mysqli_connect('localhost', 'root', '', 'car_insurance') or die('Error connecting to database');
 
+if (!empty($_POST)) {
+    $key = $_POST['key'];
+    if ($key == 'select_make') {
+        make_option();
+    } else if ($key == 'select_model') {
+        model_option($_POST['model']);
+    } else if ($key == 'select_sub') {
+        sub_model($_POST['sub']);
+    } else if ($key == 'output') {
+        get_car($_POST['make'], $_POST['model'], $_POST['sub'], $_POST['year']);
+    }
+}
+
 function model_option($make)
 {
     $db = $GLOBALS['db'];
-    $sql = "SELECT DISTINCT MODEL FROM TBL_MODEL_TYPE WHERE MAKE like \"$make\" ORDER BY MODEL";
+    $sql = "SELECT DISTINCT MODEL_ID, MODEL FROM TBL_MODEL_TYPE WHERE MAKE like \"$make\" GROUP BY MODEL";
     $res = mysqli_query($db, $sql);
+    $models = array();
     while ($row = mysqli_fetch_assoc($res)) {
-        echo "<option value='" . $row['CAR_ID'] . "'>" . $row['MODEL'] . "</option>";
+        $models[] = array('carid' => $row['MODEL_ID'], 'model' => $row['MODEL']);
     }
+    echo json_encode($models);
 }
 
 function make_option()
 {
     $db = $GLOBALS['db'];
-    $sql = "SELECT DISTINCT MAKE FROM TBL_MODEL_TYPE ORDER BY MAKE";
+    $sql = "SELECT DISTINCT MODEL_ID, MAKE FROM TBL_MODEL_TYPE GROUP BY MAKE";
     $res = mysqli_query($db, $sql);
+    $makes = array();
     while ($row = mysqli_fetch_assoc($res)) {
-        echo "<option value='" . $row['CAR_ID'] . "'>" . $row['MAKE'] . "</option>";
+        $makes[] = array('carid' => $row['MODEL_ID'], 'make' => $row['MAKE']);
     }
+    echo json_encode($makes);
 }
 
 function sub_model($model)
 {
     $db = $GLOBALS['db'];
-    $sql = "SELECT SUB_MODEL FROM TBL_MODEL_TYPE WHERE MODEL like \"$model\" ORDER BY SUB_MODEL";
+    $sql = "SELECT MODEL_ID, SUB_MODEL FROM TBL_MODEL_TYPE WHERE MODEL like \"$model\" GROUP BY SUB_MODEL";
     $res = mysqli_query($db, $sql);
+    $sub = array();
     while ($row = mysqli_fetch_assoc($res)) {
-        echo "<option value='" . $row['CAR_ID'] . "'>" . $row['SUB_MODEL'] . "</option>";
+        $sub[] = array('carid' => $row['MODEL_ID'], 'submodel' => $row['SUB_MODEL']);
     }
+    echo json_encode($sub);
+}
+
+function get_car($make, $model, $sub, $year)
+{
+    $db = $GLOBALS['db'];
+    $sql = "SELECT COVERAGE_ID, REGISTER_YEAR, COVERAGE_MIN, COVERAGE_MAX, COVERAGE_MIN_2, COVERAGE_MAX_2, PREMIUMS, MAKE, MODEL, SUB_MODEL, INSURE_COMPANY 
+FROM TBL_COVERAGE C, TBL_INSURE I, TBL_MODEL_TYPE T 
+WHERE C.INSURE_ID = I.URN AND C.MODEL_ID = T.MODEL_ID AND T.MODEL_ID IN (SELECT MODEL_ID FROM TBL_MODEL_TYPE T WHERE MAKE like \"$make\" AND MODEL LIKE \"$model\" AND SUB_MODEL LIKE  \"$sub\") AND PREMIUMS > 0 AND REGISTER_YEAR = $year";
+    $res = mysqli_query($db, $sql);
+    $output = array();
+    while ($row = mysqli_fetch_assoc($res)) {
+        $output[] = array('cov_id' => $row['COVERAGE_ID'], 'make' => $row['MAKE'], 'model' => $row['MODEL'],
+            'sub' => $row['SUB_MODEL'], 'insure' => $row['INSURE_COMPANY'], 'year' => $row['REGISTER_YEAR'],
+            'min' => $row['COVERAGE_MIN'], 'max' => $row['COVERAGE_MAX'], 'premiums' => $row['PREMIUMS'],
+            'min_2' => $row['COVERAGE_MIN_2'], 'max_2' => $row['COVERAGE_MAX_2']);
+    }
+    echo json_encode($output);
 }
